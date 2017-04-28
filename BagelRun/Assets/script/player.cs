aa55de327502db7aeb849955084eb678e93e10a7
kight-caym <amenohabagiri0424@yahoo.co.jp>
2017-04-28 17:33:03 +0900

    controller_create

---
+++ BagelRun/Assets/script/player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.gameObject.transform.Rotate (0, 0, -5);
+
+	}
+	void OnCollisionEnter2D(Collision2D col){
+		if (col.gameObject.tag == "stage") {
+			PlayerPrefs.SetInt ("jumpping", 1);
+		}
+	}
+}
