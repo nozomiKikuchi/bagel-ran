f670be0d788569d34b8947252b35da77fada531a
Daiki Tamiya <daikitamiya421@gmai.com>
2017-06-02 18:00:21 +0900

    fix

--- BagelRun/Assets/script/player.cs
+++ BagelRun/Assets/script/player.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class player : MonoBehaviour {
-	
+
+//	public GameObject bacon;
+//	public GameObject lettuce;
+//	public GameObject tomato;
 	// Use this for initialization
 	void Start () {
 		
@@ -12,11 +15,14 @@ public class player : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		this.gameObject.transform.Rotate (0, 0, -6);
+		print (this.transform.position);
 
 	}
 	void OnCollisionEnter2D(Collision2D col){
 		if (col.gameObject.tag == "stage") {
 			PlayerPrefs.SetInt ("jumpping", 1);
+		} else if (col.gameObject.tag == "Item") {
+			Destroy (col.gameObject);
 		}
 	}
 }
