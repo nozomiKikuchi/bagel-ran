65549e987bb7c79411056b216dfa30d7be7768d4
Daiki Tamiya <daikitamiya421@gmai.com>
2017-06-03 23:21:50 +0900

    add StartScene

---
+++ BagelRun/Assets/script/UFOController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UFOController : MonoBehaviour {
+
+	public GameObject ufo;
+	public GameObject blackBack;
+	// Use this for initialization
+	void Start(){
+		PlayerPrefs.SetInt ("isUFOCome", 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		print (PlayerPrefs.GetInt ("isUFOCome"));
+		if (PlayerPrefs.GetInt ("isUFOCome") == 2) {
+			ufo.transform.position += new Vector3(3f*Time.deltaTime, 0f, 0f);
+			blackBack.GetComponent<SpriteRenderer> ().color += new Color(0, 0, 0, 1/255f);
+		} else if (PlayerPrefs.GetInt ("isUFOCome") == 0) {
+			if (ufo.transform.position.x >= 3.5) {
+				ufo.transform.position += new Vector3(0f, 0f, 0f);
+				PlayerPrefs.SetInt ("isUFOCome", 1);
+			} else {
+				ufo.transform.position += new Vector3(3f*Time.deltaTime, 0f, 0f);
+
+
+			}
+		} else {
+		}
+
+	}
+}
---
+++ BagelRun/Assets/script/WheatController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheatController : MonoBehaviour {
+
+	public GameObject wheat;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (PlayerPrefs.GetInt ("isUFOCome") == 1) {
+			if (wheat.transform.position.y >= 2.8) {
+				wheat.transform.position += new Vector3 (0f, 0f, 0f);
+				PlayerPrefs.SetInt ("isUFOCome", 2);
+				wheat.SetActive (false);
+			} else {
+				wheat.transform.position += new Vector3 (0f, 3f * Time.deltaTime, 0f);
+			}
+		} else {
+			
+		}
+	}
+}
