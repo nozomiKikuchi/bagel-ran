b99200dd4ab7a2dcf2dda674196ec3f5252d09b4
Daiki Tamiya <daikitamiya421@gmai.com>
2017-06-19 20:52:01 +0900

    making start scene

---
+++ BagelRun/BagelRun/Classes/Native/AssemblyU2DCSharp_gameOver1155260296.h
@@ -0,0 +1,69 @@
+﻿#pragma once
+
+#include "il2cpp-config.h"
+
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+#include <stdint.h>
+
+#include "UnityEngine_UnityEngine_MonoBehaviour1158329972.h"
+
+// UnityEngine.GameObject
+struct GameObject_t1756533147;
+
+
+
+
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+
+// gameOver
+struct  gameOver_t1155260296  : public MonoBehaviour_t1158329972
+{
+public:
+	// UnityEngine.GameObject gameOver::text
+	GameObject_t1756533147 * ___text_2;
+	// UnityEngine.GameObject gameOver::reset
+	GameObject_t1756533147 * ___reset_3;
+	// UnityEngine.GameObject gameOver::home
+	GameObject_t1756533147 * ___home_4;
+
+public:
+	inline static int32_t get_offset_of_text_2() { return static_cast<int32_t>(offsetof(gameOver_t1155260296, ___text_2)); }
+	inline GameObject_t1756533147 * get_text_2() const { return ___text_2; }
+	inline GameObject_t1756533147 ** get_address_of_text_2() { return &___text_2; }
+	inline void set_text_2(GameObject_t1756533147 * value)
+	{
+		___text_2 = value;
+		Il2CppCodeGenWriteBarrier(&___text_2, value);
+	}
+
+	inline static int32_t get_offset_of_reset_3() { return static_cast<int32_t>(offsetof(gameOver_t1155260296, ___reset_3)); }
+	inline GameObject_t1756533147 * get_reset_3() const { return ___reset_3; }
+	inline GameObject_t1756533147 ** get_address_of_reset_3() { return &___reset_3; }
+	inline void set_reset_3(GameObject_t1756533147 * value)
+	{
+		___reset_3 = value;
+		Il2CppCodeGenWriteBarrier(&___reset_3, value);
+	}
+
+	inline static int32_t get_offset_of_home_4() { return static_cast<int32_t>(offsetof(gameOver_t1155260296, ___home_4)); }
+	inline GameObject_t1756533147 * get_home_4() const { return ___home_4; }
+	inline GameObject_t1756533147 ** get_address_of_home_4() { return &___home_4; }
+	inline void set_home_4(GameObject_t1756533147 * value)
+	{
+		___home_4 = value;
+		Il2CppCodeGenWriteBarrier(&___home_4, value);
+	}
+};
+
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
---
+++ BagelRun/BagelRun/Classes/Native/Il2CppCompilerCalculateTypeValues_17Table.cpp
@@ -0,0 +1,89 @@
+﻿#include "il2cpp-config.h"
+
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+#include <cstring>
+#include <string.h>
+#include <stdio.h>
+#include <cmath>
+#include <limits>
+#include <assert.h>
+
+#include "class-internals.h"
+#include "codegen/il2cpp-codegen.h"
+#include "UnityEngine_UI_UnityEngine_UI_Outline1417504278.h"
+#include "UnityEngine_UI_UnityEngine_UI_PositionAsUV11102546563.h"
+#include "UnityEngine_UI_UnityEngine_UI_Shadow4269599528.h"
+#include "UnityEngine_UI_U3CPrivateImplementationDetailsU3E1486305137.h"
+#include "UnityEngine_UI_U3CPrivateImplementationDetailsU3E_1568637717.h"
+#include "AssemblyU2DCSharp_U3CModuleU3E3783534214.h"
+#include "AssemblyU2DCSharp_button1265659864.h"
+#include "AssemblyU2DCSharp_control2399976437.h"
+#include "AssemblyU2DCSharp_gameOver1155260296.h"
+#include "AssemblyU2DCSharp_moveStage2931930111.h"
+#include "AssemblyU2DCSharp_player1147742565.h"
+#include "AssemblyU2DCSharp_stage_create365957765.h"
+
+
+
+
+
+
+
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1700 = { 0, -1, 0, 0 };
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1701 = { 0, -1, 0, 0 };
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1702 = { sizeof (Outline_t1417504278), -1, 0, 0 };
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1703 = { sizeof (PositionAsUV1_t1102546563), -1, 0, 0 };
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1704 = { sizeof (Shadow_t4269599528), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1704[4] = 
+{
+	Shadow_t4269599528::get_offset_of_m_EffectColor_3(),
+	Shadow_t4269599528::get_offset_of_m_EffectDistance_4(),
+	Shadow_t4269599528::get_offset_of_m_UseGraphicAlpha_5(),
+	0,
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1705 = { sizeof (U3CPrivateImplementationDetailsU3E_t1486305141), -1, sizeof(U3CPrivateImplementationDetailsU3E_t1486305141_StaticFields), 0 };
+extern const int32_t g_FieldOffsetTable1705[1] = 
+{
+	U3CPrivateImplementationDetailsU3E_t1486305141_StaticFields::get_offset_of_U24fieldU2D7BBE37982E6C057ED87163CAFC7FD6E5E42EEA46_0(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1706 = { sizeof (U24ArrayTypeU3D12_t1568637717)+ sizeof (Il2CppObject), sizeof(U24ArrayTypeU3D12_t1568637717 ), 0, 0 };
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1707 = { sizeof (U3CModuleU3E_t3783534221), -1, 0, 0 };
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1708 = { sizeof (button_t1265659864), -1, 0, 0 };
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1709 = { sizeof (control_t2399976437), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1709[1] = 
+{
+	control_t2399976437::get_offset_of_bagel_2(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1710 = { sizeof (gameOver_t1155260296), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1710[3] = 
+{
+	gameOver_t1155260296::get_offset_of_text_2(),
+	gameOver_t1155260296::get_offset_of_reset_3(),
+	gameOver_t1155260296::get_offset_of_home_4(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1711 = { sizeof (moveStage_t2931930111), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1711[3] = 
+{
+	moveStage_t2931930111::get_offset_of_speed_2(),
+	moveStage_t2931930111::get_offset_of_startPosition_3(),
+	moveStage_t2931930111::get_offset_of_endPosition_4(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1712 = { sizeof (player_t1147742565), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1712[1] = 
+{
+	player_t1147742565::get_offset_of__rigid_2(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1713 = { sizeof (stage_create_t365957765), -1, 0, 0 };
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
