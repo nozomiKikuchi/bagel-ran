5aabcf70076ea33202a5671817512c4f1e51ece4
Daiki Tamiya <daikitamiya421@gmai.com>
2017-07-06 20:00:08 +0900

    ステータスバー追加

---
+++ BagelRun/Assets/script/SliderController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderController : MonoBehaviour {
+
+	public Slider slider;
+	float param = 0;
+	public float max;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (param > max) {
+		} else {
+			param += 0.2f;
+			// HPゲージに値を設定
+			slider.value = param;
+		}
+	}
+}
