6439f1684a93f672acd7fb732ce25807cc616b91
Daiki Tamiya <daikitamiya421@gmai.com>
2017-07-16 01:55:05 +0900

    セレクト画面スライド機能追加、リザルト画面ベーグルが落ちてくるようにした

---
+++ BagelRun/Temp/il2cppOutput/il2cppOutput/Il2CppCompilerCalculateTypeValues_19Table.cpp
@@ -0,0 +1,260 @@
+﻿#include "il2cpp-config.h"
+
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+#include <cstring>
+#include <string.h>
+#include <stdio.h>
+#include <cmath>
+#include <limits>
+#include <assert.h>
+
+#include "class-internals.h"
+#include "codegen/il2cpp-codegen.h"
+#include "UnityEngine_UI_UnityEngine_UI_VertexHelper385374196.h"
+#include "UnityEngine_UI_UnityEngine_UI_BaseVertexEffect2504093552.h"
+#include "UnityEngine_UI_UnityEngine_UI_BaseMeshEffect1728560551.h"
+#include "UnityEngine_UI_UnityEngine_UI_Outline1417504278.h"
+#include "UnityEngine_UI_UnityEngine_UI_PositionAsUV11102546563.h"
+#include "UnityEngine_UI_UnityEngine_UI_Shadow4269599528.h"
+#include "UnityEngine_UI_U3CPrivateImplementationDetailsU3E1486305137.h"
+#include "UnityEngine_UI_U3CPrivateImplementationDetailsU3E_1568637717.h"
+#include "UnityEngine_Analytics_U3CModuleU3E3783534214.h"
+#include "UnityEngine_Analytics_UnityEngine_Analytics_Analyt2191537572.h"
+#include "UnityEngine_Analytics_UnityEngine_Analytics_Analyt1068911718.h"
+#include "UnityEngine_Analytics_UnityEngine_Analytics_Tracka1304606600.h"
+#include "UnityEngine_Analytics_UnityEngine_Analytics_Tracka2256174789.h"
+#include "AssemblyU2DCSharp_U3CModuleU3E3783534214.h"
+#include "AssemblyU2DCSharp_FadeManager2205509615.h"
+#include "AssemblyU2DCSharp_FlickController77099233.h"
+#include "AssemblyU2DCSharp_MainSceneButtonController3316297225.h"
+#include "AssemblyU2DCSharp_MainStart3226110189.h"
+#include "AssemblyU2DCSharp_MoveCamera3193441886.h"
+#include "AssemblyU2DCSharp_MovePanel3417660653.h"
+#include "AssemblyU2DCSharp_SliderController2812089311.h"
+#include "AssemblyU2DCSharp_StartSceneButtonController2644416436.h"
+#include "AssemblyU2DCSharp_UfoControllerScript1959917385.h"
+#include "AssemblyU2DCSharp_control2399976437.h"
+#include "AssemblyU2DCSharp_gameOver1155260296.h"
+#include "AssemblyU2DCSharp_itemGet2266050241.h"
+#include "AssemblyU2DCSharp_moveStage2931930111.h"
+#include "AssemblyU2DCSharp_player1147742565.h"
+#include "AssemblyU2DCSharp_stage_create365957765.h"
+
+
+
+
+
+
+
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1900 = { sizeof (VertexHelper_t385374196), -1, sizeof(VertexHelper_t385374196_StaticFields), 0 };
+extern const int32_t g_FieldOffsetTable1900[11] = 
+{
+	VertexHelper_t385374196::get_offset_of_m_Positions_0(),
+	VertexHelper_t385374196::get_offset_of_m_Colors_1(),
+	VertexHelper_t385374196::get_offset_of_m_Uv0S_2(),
+	VertexHelper_t385374196::get_offset_of_m_Uv1S_3(),
+	VertexHelper_t385374196::get_offset_of_m_Uv2S_4(),
+	VertexHelper_t385374196::get_offset_of_m_Uv3S_5(),
+	VertexHelper_t385374196::get_offset_of_m_Normals_6(),
+	VertexHelper_t385374196::get_offset_of_m_Tangents_7(),
+	VertexHelper_t385374196::get_offset_of_m_Indices_8(),
+	VertexHelper_t385374196_StaticFields::get_offset_of_s_DefaultTangent_9(),
+	VertexHelper_t385374196_StaticFields::get_offset_of_s_DefaultNormal_10(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1901 = { sizeof (BaseVertexEffect_t2504093552), -1, 0, 0 };
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1902 = { sizeof (BaseMeshEffect_t1728560551), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1902[1] = 
+{
+	BaseMeshEffect_t1728560551::get_offset_of_m_Graphic_2(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1903 = { 0, -1, 0, 0 };
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1904 = { 0, -1, 0, 0 };
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1905 = { sizeof (Outline_t1417504278), -1, 0, 0 };
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1906 = { sizeof (PositionAsUV1_t1102546563), -1, 0, 0 };
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1907 = { sizeof (Shadow_t4269599528), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1907[4] = 
+{
+	Shadow_t4269599528::get_offset_of_m_EffectColor_3(),
+	Shadow_t4269599528::get_offset_of_m_EffectDistance_4(),
+	Shadow_t4269599528::get_offset_of_m_UseGraphicAlpha_5(),
+	0,
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1908 = { sizeof (U3CPrivateImplementationDetailsU3E_t1486305142), -1, sizeof(U3CPrivateImplementationDetailsU3E_t1486305142_StaticFields), 0 };
+extern const int32_t g_FieldOffsetTable1908[1] = 
+{
+	U3CPrivateImplementationDetailsU3E_t1486305142_StaticFields::get_offset_of_U24fieldU2D7BBE37982E6C057ED87163CAFC7FD6E5E42EEA46_0(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1909 = { sizeof (U24ArrayTypeU3D12_t1568637717)+ sizeof (Il2CppObject), sizeof(U24ArrayTypeU3D12_t1568637717 ), 0, 0 };
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1910 = { sizeof (U3CModuleU3E_t3783534221), -1, 0, 0 };
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1911 = { sizeof (AnalyticsTracker_t2191537572), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1911[5] = 
+{
+	AnalyticsTracker_t2191537572::get_offset_of_m_EventName_2(),
+	AnalyticsTracker_t2191537572::get_offset_of_m_Dict_3(),
+	AnalyticsTracker_t2191537572::get_offset_of_m_PrevDictHash_4(),
+	AnalyticsTracker_t2191537572::get_offset_of_m_TrackableProperty_5(),
+	AnalyticsTracker_t2191537572::get_offset_of_m_Trigger_6(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1912 = { sizeof (Trigger_t1068911718)+ sizeof (Il2CppObject), sizeof(int32_t), 0, 0 };
+extern const int32_t g_FieldOffsetTable1912[8] = 
+{
+	Trigger_t1068911718::get_offset_of_value___1() + static_cast<int32_t>(sizeof(Il2CppObject)),
+	0,
+	0,
+	0,
+	0,
+	0,
+	0,
+	0,
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1913 = { sizeof (TrackableProperty_t1304606600), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1913[2] = 
+{
+	0,
+	TrackableProperty_t1304606600::get_offset_of_m_Fields_1(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1914 = { sizeof (FieldWithTarget_t2256174789), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1914[6] = 
+{
+	FieldWithTarget_t2256174789::get_offset_of_m_ParamName_0(),
+	FieldWithTarget_t2256174789::get_offset_of_m_Target_1(),
+	FieldWithTarget_t2256174789::get_offset_of_m_FieldPath_2(),
+	FieldWithTarget_t2256174789::get_offset_of_m_TypeString_3(),
+	FieldWithTarget_t2256174789::get_offset_of_m_DoStatic_4(),
+	FieldWithTarget_t2256174789::get_offset_of_m_StaticString_5(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1915 = { sizeof (U3CModuleU3E_t3783534222), -1, 0, 0 };
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1916 = { sizeof (FadeManager_t2205509615), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1916[5] = 
+{
+	FadeManager_t2205509615::get_offset_of_speed_2(),
+	FadeManager_t2205509615::get_offset_of_alfa_3(),
+	FadeManager_t2205509615::get_offset_of_red_4(),
+	FadeManager_t2205509615::get_offset_of_green_5(),
+	FadeManager_t2205509615::get_offset_of_blue_6(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1917 = { sizeof (FlickController_t77099233), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1917[16] = 
+{
+	FlickController_t77099233::get_offset_of_minSwipeDistX_2(),
+	FlickController_t77099233::get_offset_of_minSwipeDistY_3(),
+	FlickController_t77099233::get_offset_of_startPos_4(),
+	FlickController_t77099233::get_offset_of_endPos_5(),
+	FlickController_t77099233::get_offset_of_swipeDistX_6(),
+	FlickController_t77099233::get_offset_of_swipeDistY_7(),
+	FlickController_t77099233::get_offset_of_SignValueX_8(),
+	FlickController_t77099233::get_offset_of_SignValueY_9(),
+	FlickController_t77099233::get_offset_of_nomalBagel_10(),
+	FlickController_t77099233::get_offset_of_greenBagel_11(),
+	FlickController_t77099233::get_offset_of_purpleBagel_12(),
+	FlickController_t77099233::get_offset_of_rightArrow_13(),
+	FlickController_t77099233::get_offset_of_leftArrow_14(),
+	FlickController_t77099233::get_offset_of_weight_15(),
+	FlickController_t77099233::get_offset_of_speed_16(),
+	FlickController_t77099233::get_offset_of_luck_17(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1918 = { sizeof (MainSceneButtonController_t3316297225), -1, 0, 0 };
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1919 = { sizeof (MainStart_t3226110189), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1919[4] = 
+{
+	MainStart_t3226110189::get_offset_of_bagel_2(),
+	MainStart_t3226110189::get_offset_of_ufo_3(),
+	MainStart_t3226110189::get_offset_of_uforight_4(),
+	MainStart_t3226110189::get_offset_of_start_5(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1920 = { sizeof (MoveCamera_t3193441886), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1920[9] = 
+{
+	MoveCamera_t3193441886::get_offset_of_nomalBagel_2(),
+	MoveCamera_t3193441886::get_offset_of_greenBagel_3(),
+	MoveCamera_t3193441886::get_offset_of_purpleBagel_4(),
+	MoveCamera_t3193441886::get_offset_of_look_5(),
+	MoveCamera_t3193441886::get_offset_of_rightArrow_6(),
+	MoveCamera_t3193441886::get_offset_of_leftArrow_7(),
+	MoveCamera_t3193441886::get_offset_of_weight_8(),
+	MoveCamera_t3193441886::get_offset_of_speed_9(),
+	MoveCamera_t3193441886::get_offset_of_luck_10(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1921 = { sizeof (MovePanel_t3417660653), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1921[2] = 
+{
+	0,
+	MovePanel_t3417660653::get_offset_of__isRendered_3(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1922 = { sizeof (SliderController_t2812089311), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1922[5] = 
+{
+	SliderController_t2812089311::get_offset_of_slider_2(),
+	SliderController_t2812089311::get_offset_of_param_3(),
+	SliderController_t2812089311::get_offset_of_max1_4(),
+	SliderController_t2812089311::get_offset_of_max2_5(),
+	SliderController_t2812089311::get_offset_of_max3_6(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1923 = { sizeof (StartSceneButtonController_t2644416436), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1923[6] = 
+{
+	StartSceneButtonController_t2644416436::get_offset_of_bagel_2(),
+	StartSceneButtonController_t2644416436::get_offset_of_run_3(),
+	StartSceneButtonController_t2644416436::get_offset_of_currentRemainTime_4(),
+	StartSceneButtonController_t2644416436::get_offset_of_spRenderer_5(),
+	StartSceneButtonController_t2644416436::get_offset_of_fadeTime_6(),
+	StartSceneButtonController_t2644416436::get_offset_of_sound01_7(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1924 = { sizeof (UfoControllerScript_t1959917385), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1924[10] = 
+{
+	UfoControllerScript_t1959917385::get_offset_of_ufo_2(),
+	UfoControllerScript_t1959917385::get_offset_of_wheat1_3(),
+	UfoControllerScript_t1959917385::get_offset_of_wheat2_4(),
+	UfoControllerScript_t1959917385::get_offset_of_uforight_5(),
+	UfoControllerScript_t1959917385::get_offset_of_speed_6(),
+	UfoControllerScript_t1959917385::get_offset_of_radius_7(),
+	UfoControllerScript_t1959917385::get_offset_of_yPosition_8(),
+	UfoControllerScript_t1959917385::get_offset_of_x_9(),
+	UfoControllerScript_t1959917385::get_offset_of_y_10(),
+	UfoControllerScript_t1959917385::get_offset_of_z_11(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1925 = { sizeof (control_t2399976437), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1925[1] = 
+{
+	control_t2399976437::get_offset_of_bagel_2(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1926 = { sizeof (gameOver_t1155260296), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1926[3] = 
+{
+	gameOver_t1155260296::get_offset_of_text_2(),
+	gameOver_t1155260296::get_offset_of_reset_3(),
+	gameOver_t1155260296::get_offset_of_home_4(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1927 = { sizeof (itemGet_t2266050241), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1927[1] = 
+{
+	itemGet_t2266050241::get_offset_of_player_2(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1928 = { sizeof (moveStage_t2931930111), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1928[4] = 
+{
+	moveStage_t2931930111::get_offset_of_speed_2(),
+	moveStage_t2931930111::get_offset_of_startPosition_3(),
+	moveStage_t2931930111::get_offset_of_endPosition_4(),
+	moveStage_t2931930111::get_offset_of_start_5(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1929 = { sizeof (player_t1147742565), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1929[1] = 
+{
+	player_t1147742565::get_offset_of__rigid_2(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1930 = { sizeof (stage_create_t365957765), -1, 0, 0 };
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
